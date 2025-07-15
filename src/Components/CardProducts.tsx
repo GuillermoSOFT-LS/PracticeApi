078595c7afe897851f204b45ee62e03024efd811
GuillermoSOFT-LS <guillermolarasoft@gmail.com>
2025-07-15 09:21:48 -0400

    Se soluciono el scroll y se refactorizo el codigo

---
+++ src/Components/CardProducts.tsx
@@ -0,0 +1,27 @@
+import {Text, View} from "react-native";
+import {Image} from "expo-image";
+import {AntDesign} from "@expo/vector-icons";
+
+
+const  CardProducts = ({item}:any) => {
+    return (
+        <View className='flex justify-center w-[50%] p-2  pb-10'>
+            <View className='p-2 bg-white rounded-2xl shadow-sm'>
+                <View className='flex-1 items-center'>
+                    <Image style={{resizeMode: 'contain'}} className='w-28 h-36 resize-contain' source={{uri: item.image}}/>
+                    <Text className='font-bold pb-3 pt-2'>{item.title}</Text>
+                    <Text className='' numberOfLines={3}>{item.description}</Text>
+                </View>
+                <View className='flex-row justify-between items-center pt-2'>
+                    <Text className='font-bold pl-3'>US${item.price}</Text>
+                    <View className='flex-row items-center'>
+                        <AntDesign name="star" size={20} color="#f0a803"/>
+                        <Text className='font-bold'>{item.rating.rate}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default CardProducts;
--- src/app/Screen/index.tsx
+++ src/app/Screen/index.tsx
@@ -1,52 +1,30 @@
-import {FlatList, ScrollView, Text, View} from "react-native";
+import {FlatList, View} from "react-native";
 import {useEffect, useState} from "react";
 import {fetchData} from "@/src/core/api/FechApi";
 import {IProducts} from "@/src/interfaces/products.interfaces";
-import {Image} from "expo-image";
-import {AntDesign} from "@expo/vector-icons";
+import CardProducts from "@/src/Components/CardProducts";
 
-const HomeProduct = () => {
+export  default function HomeProduct() {
 
     const [products, setProducts] = useState<IProducts[]>([])
 
     useEffect(() => {
-        const loadedProducts = async ()=> {
+        const LoadedProducts = async ()=> {
             const date = await fetchData<IProducts[]>('products')
             setProducts(date)
         }
-        loadedProducts()
+        LoadedProducts()
     }, []);
 
-    let renderItem = ({item}:any) => {
-        return (
-            <View className='flex justify-center w-[50%] p-2  pb-10'>
-                <View className='p-2 bg-white rounded-2xl shadow-sm'>
-                    <View className='flex-1 items-center'>
-                        <Image className='w-28 h-36 resize-contain' source={{uri: item.image}}/>
-                        <Text className='font-bold pb-3 pt-2'>{item.title}</Text>
-                        <Text className='' numberOfLines={3}>{item.description}</Text>
-                    </View>
-                    <View className='flex-row justify-between items-center pt-2'>
-                        <Text className='font-bold pl-3'>US${item.price}</Text>
-                        <View className='flex-row items-center'>
-                            <AntDesign name="star" size={20} color="#f0a803"/>
-                            <Text className='font-bold'>{item.rating.rate}</Text>
-                        </View>
-                    </View>
-                </View>
-            </View>
-        );
-    };
+
     return (
-            <View className='p-2'>
+            <View style={{flex: 1}} className='p-2'>
                 <FlatList
                     data={products}
                     numColumns={2}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={renderItem}
+                    renderItem={CardProducts}
                 />
             </View>
     )
 }
-
-export default HomeProduct
